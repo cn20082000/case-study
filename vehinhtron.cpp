648d67ac9309d4dcc6f2937eb1a4b7987844c3ab
minhtuan72 <minhtuan070220@gmail.com>
2022-12-18 15:49:30 +0700

    them ngoai le

--- vehinhtron.cpp
+++ vehinhtron.cpp
@@ -2,12 +2,12 @@
 
 using namespace std;
 
-int r, x, y;
-
+string bankinh, toadox, toadoy;
+ int x, y, r;
 
 int check(int a, int b){
 	int tmp;
-	tmp = (a-x)*(a-x)+ (b-y)*(b-y) -r*r;
+	tmp = (a-x)*(a-x)+ (b-y)*(b-y) - r*r;
 	if(tmp<=0) return 1;
 	return 0;	
 }
@@ -27,24 +27,53 @@ void in(){
 
 	
 }
+int chuyen(string s){
+	int tmp =0;
+	int i=0;
+	while(i<s.length()){
+		int t = s[i]-'0';
+		if(0<=t && t<=9){
+			tmp = tmp*10 + t;
+			i++;
+		}
+		else{
+			return -1;
+		}
+	}
+	return tmp;
+}
 int main()
 {
  
    cout<<"nhap toa do tam va ban kinh hinh tron: "<<endl;
-   cout<<"x: ";
-   cin>>x;
- //  cout<<endl;
-   cout<<"y: ";
-   cin>>y;
-   //cout<<endl;
-   cout<<"r: ";
-   cin>>r; 
-  // cout<<endl;
+   cout<<"toa do x: ";
+   cin>>toadox;
+
+   cout<<"toa do y: ";
+   cin>>toadoy;
+
+   cout<<"ban kinh r: ";
+   cin>>bankinh; 
+  	
+  	
+  
+   
+   if(chuyen(toadox)==-1|| chuyen(toadoy)==-1|| chuyen(bankinh)==-1){
+   		cout<<"nhap vao khong hop le "<<endl;
+   		return 0;
+   }
+   else{
+   		x = chuyen(toadox);
+		y = chuyen(toadoy);
+		r = chuyen(bankinh);	
+   }
+   
    if(x*x<r*r||y*y<r*r){
    	 cout<<"toa do ban kinh khong hop le"<<endl;
    }
    else{
    		in();
    }
+	
     return 0;
 }
